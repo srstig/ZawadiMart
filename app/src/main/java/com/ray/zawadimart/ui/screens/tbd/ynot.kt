ca111652775a853c8b03d86ada448fcc7435d835
srstig <rayogutu36@gmail.com>
2025-04-08 16:36:56 +0300

    second

---
+++ app/src/main/java/com/ray/zawadimart/ui/screens/tbd/ynot.kt
@@ -0,0 +1,19 @@
+package com.ray.zawadimart.ui.screens.tbd
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AboutScreen(navController: NavController){
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun AboutScreenPreview(){
+
+    AboutScreen(rememberNavController())
+}
